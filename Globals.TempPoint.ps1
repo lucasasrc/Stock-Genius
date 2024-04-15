f96470cfd60f3b4b25eaa342238a731ba4bde995
lucasasrc <lucasasrc@gmail.com>
2024-04-14 21:28:47 -0300

    CRUDs
    Implementação de filto de usuários e opção de deletar no banco.

--- Globals.TempPoint.ps1
+++ Globals.TempPoint.ps1
@@ -6,10 +6,7 @@
 $mfaCode = ''
 $usuarioAutenticado = ''
 
-$usuarioListado = ''
-$nomeListado = ''
-$emailListado = ''
-$tipoListado = ''
+[System.Windows.Forms.Screen]::AllScreens[0].bounds.width - ($f.bounds.Width + 1)
 
 #Configurações de acesso ao banco de dados
 Add-Type -Path "C:\Program Files (x86)\MySQL\MySQL Connector NET 8.3.0\MySql.Data.dll"
